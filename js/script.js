35b5f93da4ea316502895478ade8aad986e8b73b
Mykhailo32 <codi385@gmail.com>
2021-04-03 23:13:32 +0300

    ДЗ1 task №2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-// Задача 3.7 Найти в массиве все серии подряд идущих одинаковых элементов и удалить из них все элементы кроме одного.
-let arr = [5, 5, 5, 4, 5, 6, 6, 12, 12, 12, 12, 2, 1, 4, 6, 8, 8, 8, 15, 1, 11, 11, 11, 15,]
-document.write(`${arr}<br>`)
-let newArr = arr.filter((val, i, arr) => {
-    if (val != arr[i + 1])
-        return val
-})
-document.write(newArr)
+//Задача 2. Дано масив цін. Сформувати новий масив, який містить тільки ті ціни, які менші за задану.
+
+let price =[200,90,50,270,180,20]
+let priceStart = 100
+let newPrice = price.filter(x=>x<100)
+document.write(newPrice)
