b0804c040d7fb592c8d5e1af17681a32ef2a4645
Mykhailo32 <codi385@gmail.com>
2021-04-04 18:05:10 +0300

    Задачі для масивів/ task №2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-//Напишите функцию copyArr(arr), которая копирует массив не изменяя иригинал.
-let vegetables = ['Капуста', 'Репа', 'Редиска', 'Морковка'];
-let shallowCopy = vegetables.slice();
-document.write(`${vegetables},<br>${shallowCopy} </b>`)
+/*Напишите код, который преобразовывает и объединяет все элементы массива в одно строковое значение. 
+Элементы массива будут разделены запятой. Получите результат двумя разными методами.*/
 
+let vegetables = ['Капуста', 'Репа', 'Редиска', 'Морковка'];
+str1= vegetables.join()
+str2= vegetables.toString();
+
+document.write(str1)
+document.write(str2)
